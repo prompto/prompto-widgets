0f224ba600f415c837cbec301eb84159390a1bc1
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-03 20:07:16 +0800

    use patched react-bootstrap

--- react-bootstrap-4/src/LibraryGenerator.js
+++ react-bootstrap-4/src/LibraryGenerator.js
@@ -1,10 +1,12 @@
 // to run this script you must go through the following steps
 // from the widget-library-generator project, run 'npm link'
-// this will publish the widget-library-generator on your box
-// then from this project, run 'npm link widget-library-generator'
+// this will publish the widget-library-generator package on your box
+// from the react-bootstrap project, use the 'do-not-strip-prototypes' branch, then run 'npm link' (also uses a different package name)
+// this will publish the patched react-bootstrap package on your box
+// then from this project, run 'npm link widget-library-generator react-bootstrap'
 
 import { PropTypes, TypeProperty, ValueSetProperty, WidgetLibraryGenerator } from 'widget-library-generator';
-import { default as ReactBootstrap } from 'react-bootstrap';
+import { default as ReactBootstrap } from 'react-bootstrap-4';
 
 const HELPERS = {
 };
