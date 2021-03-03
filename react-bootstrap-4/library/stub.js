49c838058d4317a26480ae464c3fbabe612bec4b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-03 15:03:18 +0800

    working except for properties

--- react-bootstrap-4/library/stub.js
+++
@@ -1,2 +0,0 @@
-var React = { Component: function() { this.render = function() { return {}; }; return this; } };
-self.ReactBootstrap = {Button: React.Component, Navbar: {Brand: React.Component }, InputGroup: {render: React.Component.render, Append: React.Component } };
