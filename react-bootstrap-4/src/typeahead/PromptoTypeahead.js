9467b44c131c3e6f21b3acb05d3fcecfd073abfb
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-01-21 10:47:04 +0100

    Fix typeahead issue

--- react-bootstrap-4/src/typeahead/PromptoTypeahead.js
+++ react-bootstrap-4/src/typeahead/PromptoTypeahead.js
@@ -11,6 +11,13 @@ export default React.forwardRef((props, ref)=>{
     } else if(!Array.isArray(props.options) && props.options.toList) {
         adjustedProps.options = props.options.toList();
     }
+
+    if(!props.selected) {
+        adjustedProps.selected = [];
+    } else if(!Array.isArray(props.selected) && props.selected.toList) {
+        adjustedProps.selected = props.selected.toList();
+    }
+
     if(props.onChange) {
         adjustedProps.onChange = items => props.onChange(new window.List(false, items));
     }
